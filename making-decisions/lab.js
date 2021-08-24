39ecba034f40c951ba6ba33ec7d13eb6308d2ce1
Weston Isheim <wisheim@gmail.com>
2021-08-24 16:23:47 -0600

    initial commit

--- making-decisions/lab.js
+++ making-decisions/lab.js
@@ -149,7 +149,7 @@ console.log(changeMyMind)
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
-changeMyMind === !true;
+changeMyMind = !changeMyMind;
 
 console.log(changeMyMind)
 
